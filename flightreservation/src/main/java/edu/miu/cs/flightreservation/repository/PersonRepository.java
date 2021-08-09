3b916e2b9b4deeeba052dfae5b7bc5aa4a301431
Emmaunuel Edorh <emmanoedorh@gmail.com>
2021-08-09 00:41:13 -0500

    Rename personRepository PersonRepository and delete PersonServiceImpl at the root of the project tree

--- flightreservation/src/main/java/edu/miu/cs/flightreservation/repository/PersonRepository.java
+++ flightreservation/src/main/java/edu/miu/cs/flightreservation/repository/PersonRepository.java
@@ -2,9 +2,11 @@ package edu.miu.cs.flightreservation.repository;
 
 import edu.miu.cs.flightreservation.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.Optional;
+
 public interface PersonRepository extends JpaRepository<Person,Long> {
-   Person findPersonByUsername(String username);
+    Person findPersonByUsername(String username);
+    Person findPersonById(Long id);
+
 }
