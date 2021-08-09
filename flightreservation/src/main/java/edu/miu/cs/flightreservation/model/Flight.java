0ad23ba9d9f29385771541b81c214b4e24a32d16
kashifjawad <kashifjawad050>
2021-08-09 15:54:57 -0500

    solve rebase conflit

---
+++ flightreservation/src/main/java/edu/miu/cs/flightreservation/model/Flight.java
@@ -0,0 +1,29 @@
+package edu.miu.cs.flightreservation.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Entity
+public class Flight {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String number;
+    private int capacity;
+    private LocalDate departureTime;
+    private LocalDateTime arrivalTime;
+    @OneToMany
+    @JoinColumn
+    private List<Ticket> tickets;
+    @ManyToOne
+    private Airline airLine;
+    @ManyToOne
+    private Airport originAirport;
+    @ManyToOne
+    private Airport destinationAirport;
+}
