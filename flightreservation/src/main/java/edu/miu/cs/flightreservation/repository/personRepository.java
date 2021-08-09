0a4176df0f5cce17cdfb04dd5d4ffe16c8e1658b
mintuta <mintefikr4@gmail.com>
2021-08-08 20:25:50 -0500

    update person 2

--- flightreservation/src/main/java/edu/miu/cs/flightreservation/repository/personRepository.java
+++
@@ -1,8 +0,0 @@
-package edu.miu.cs.flightreservation.repository;
-
-import edu.miu.cs.flightreservation.model.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonRepository extends JpaRepository<Person,Long> {
-
-}
