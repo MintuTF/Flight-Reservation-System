9078862ad7a12dc6f7448148c46106d04f45714f
Emmaunuel Edorh <emmanoedorh@gmail.com>
2021-08-08 21:08:03 -0500

    Reset JpaRepository instead of PagingAndSortingRepository

--- flightreservation/src/main/java/edu/miu/cs/flightreservation/controller/AirlineController.java
+++ flightreservation/src/main/java/edu/miu/cs/flightreservation/controller/AirlineController.java
@@ -1,11 +1,16 @@
 package edu.miu.cs.flightreservation.controller;
 
+import edu.miu.cs.flightreservation.model.Airline;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/airlines")
 public class AirlineController {
-
-
 }
