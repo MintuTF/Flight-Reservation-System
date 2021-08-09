4150720257f40bd5852b8ccb54f42eee00e37f44
mintuta <mintefikr4@gmail.com>
2021-08-08 19:24:04 -0500

    update person id

--- flightreservation/src/main/java/edu/miu/cs/flightreservation/model/Person.java
+++ flightreservation/src/main/java/edu/miu/cs/flightreservation/model/Person.java
@@ -3,27 +3,27 @@ package edu.miu.cs.flightreservation.model;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
+
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-
+import javax.persistence.*;
 
 @Entity
 @Data
-@NoArgsConstructor
+@Table(name = "Person")
 public class Person {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
     private String username;
     private String password;
     private  String status;
     private String firstName;
     private String lastName;
     private  String email;
+
 
 
 
